a45a7666d6f2dd457630b03ca6957e291e30ef85
Gkennedy335 <27gkennedy@go.dsdmail.net>
2024-10-10 20:17:34 +0000

    e

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ print(str(caps_name) )
 game_mode = input('... thats an interesting name \n do you want easy or hard mode? ')
 caps_game_mode = game_mode.title()
 if caps_game_mode == ('Easy'):
-  answer_one = input('question #1 \n ---------------- \n what is the capital of assyria? ') 
+  answer_one = input('Easy it is then \n question #1 \n ---------------- \n what is the capital of assyria? ') 
 elif answer_one == ('assur') or ('Assur'):
     print('Great Job!')
 
