076505a163a4eb3f58a8a310e58271b5855019c1
Gkennedy335 <27gkennedy@go.dsdmail.net>
2024-10-10 19:59:40 +0000

    e

--- quiz.py
+++ quiz.py
@@ -1,1 +1,10 @@
+name = input('welcome to my awsome quiz game where your skills are put to the test. \n Enter your name for the record: ')
+caps_name = name.title()
+print(str(caps_name) )
+game_mode = input('... thats an interesting name \n do you want easy or hard mode? ')
+caps_game_mode = game_mode.title()
+if caps_game_mode == ('Easy'):
+  answer_one = input('question #1 \n ---------------- \n what is the capital of assyria? ') 
+elif answer_one == ('assur') or ('Assur'):
+    print('Great Job!')
 
