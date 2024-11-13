8488ecc391072d6c45967b083d0e2406fdfe2847
Gkennedy335 <27gkennedy@go.dsdmail.net>
2024-11-12 22:08:41 -0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,35 +3,34 @@ caps_name = name.title()
 ip_number_high = 4294967296
 ip_number_low = 1111111111
 correct_one = 'Assur'
+score = 0
 print(str(caps_name) )
-game_mode = input('... thats an interesting name \n do you want easy or hard mode? ')
-caps_game_mode = game_mode.title()
-if caps_game_mode == 'Easy':
- answer_one = input('Easy it is then \n question #1 \n ---------------- \n what is the capital of assyria? ') 
-if answer_one == correct_one:
-  print('correct!')
-elif caps_game_mode == 'Hard':
-  answer_one_hard = input('Hard it is then \n question #1 \n ---------------- \n what is your ip address? (no spaces or commas between the numbers) ')
-  if int(answer_one_hard) <= (ip_number_high) and int(answer_one_hard) >= (ip_number_low):
+input_one = input('... thats an interesting name \n question #1 \n ---------------- \n what is the capital of assyria?')
+if (input_one).capitalize() == (correct_one):
+    score += 1
+    print('thats Correct!')
+else: 
+    print('Incorrect.')
+    score -= 1
+input_two = input('question #2 \n ---------------- \n what is your ip address? (no spaces or commas between the numbers)')
+if int(input_two) >= ip_number_low and int(input_two) <= ip_number_high:
     print('correct!')
-  else: print('wrong')
-  answer_two_hard = input('question #2 \n ---------------- \n what is an example of a red fruit?: ')
-  if (answer_two_hard.title()) == ('strawberry') or ('rhasberry') or ('cherry') or ('apple'):
+    score += 1
+else: 
+    print('thats Incorrect.')
+    score -= 1
+input_three = input('question #3 \n ---------------- \n what is an example of a red fruit?: ')
+if (input_three).capitalize() == 'Strawberry' or 'Cherry' or 'Rhasberry' or 'Apple':
     print('correct!')
-    answer_three_hard = input('question #3 \n ---------------- \n is fortnite a good game? ')
-    if answer_three_hard.capitalize == ('yes') or ('no'):
-      print('correct!')
-      answer_four_hard = input('question #4 \n ---------------- \n what is the name of the first computer? ')
-      if answer_four_hard.capitalize == ('Analytical engine'):
-        print("correct")
-
-
-
-    else: print('so you don\'t want to play then.')
-if answer_one != correct_one.capitalize():
-  print('correct great job!')
-else: print('wrong')
-answer_two = input('question #2 \n ---------------- \n what is your age? ') 
-if answer_two >= 1 and answer_two <= 116:
-    print('good job!')
-else: print('incorrect')  
+    score += 1
+else:
+    print('incorrect.')
+    score -= 1
+input_four = input('what is the airspeed velocity of an unladen swallow (in terms of mph)')
+if int(input_four) == 24:
+    print('correct!')
+    score += 1
+else:
+    print('wrong!')
+    score -=1
+print('Thanks for taking the quiz! your final score was ' + str(score) + ' out of 4 points!')
